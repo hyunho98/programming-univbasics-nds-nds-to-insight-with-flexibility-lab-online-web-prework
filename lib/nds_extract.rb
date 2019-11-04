70781fa58cde113f219bcc63a099b9dade79d365
Alex Lee <hyunho98@gmail.com>
2019-11-04 01:15:08 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -53,7 +53,7 @@ def movies_with_director_key(name, movies_collection)
 
   while index < movies_collection.length do
     output[index] <<
-      {name => movies_collection[:title]}
+      movie_with_director_name(name, movies_collection[index])
   end
 end
 
